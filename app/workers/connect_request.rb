a55442374e7cc68239ba7071d6f70b5e22280ad3
root <root@dongaldongal.cs6ab4cloud.internal>
2016-02-19 16:41:48 +0900

    please

--- app/workers/connect_request.rb
+++ app/workers/connect_request.rb
@@ -4,5 +4,6 @@ class ConnectRequest
 	def self.request
 		Net::HTTP.get(URI('http://0.0.0.0:3000'))
 		Net::HTTP.get(URI('http://0.0.0.0:3000/dongal_notices'))
+		Net::HTTP.get(URI('http://www.google.com'))
 	end
 end
